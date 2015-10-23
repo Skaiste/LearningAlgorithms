27c92751dd15a600379f2236d85b9419fe5bdb1b
skaiste <butkute.skaiste@gmail.com>
2015-10-23 13:28:43 +0100

    Insertion sort and testing it

--- LearningAlgorithms/src/sorting/InsertionSort.java
+++ LearningAlgorithms/src/sorting/InsertionSort.java
@@ -1,9 +1,26 @@
 package sorting;
 
-public class InsertionSort {
+public class InsertionSort implements Sorting{
 
-	public InsertionSort() {
-		// TODO Auto-generated constructor stub
+	public void sortIntArray(int[] a) {
+		if (a == null || a.length < 2)
+			return;
+		
+		// sorting begins
+		for (int j = 1; j < a.length; j++) {
+			
+			int key = a[j];
+			int i = j - 1;
+			
+			while (i >= 0 && a[i] > key) {
+				a[i+1] = a[i];
+				i -= 1;
+			}
+			
+			a[i + 1] = key;
+		}
+		// sorting ends
+		
 	}
 
 }
---
+++ LearningAlgorithms/src/testing/TestingSorting.java
@@ -0,0 +1,30 @@
+package testing;
+
+import org.junit.Test;
+
+import sorting.*;
+
+import static org.junit.Assert.*;
+
+public class TestingSorting {
+
+	@Test
+	public void testInsertionSortIntArr() {
+		testIntArray(new InsertionSort());
+	}
+	
+	private void testIntArray(Sorting sorting){
+		
+		int[] array = {31, 41, 59, 26, 41, 58};
+		int[] sortedArray = {26, 31, 41, 41, 58, 59};
+		
+		// testing sorting of an unsorted array
+		sorting.sortIntArray(array);
+		assertArrayEquals("Sorting integer array has failed", array, sortedArray);
+		
+		// testing sorting of sorted array
+		array = sortedArray.clone();
+		sorting.sortIntArray(array);
+		assertArrayEquals("Sorting integer array has failed", array, sortedArray);
+	}
+}
